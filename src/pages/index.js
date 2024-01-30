fb0bd49c2f616db3768b82ee167ddf4bc39661e3
sumiyab <sumiyabazaramarjargal@gmail.com>
2024-01-30 11:48:54 +0800

    fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,16 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export default function Home(props) {
-  const { posts, posts2 } = props;
+  const { highlight, post, trending } = props;
   console.log(posts);
   console.log(posts2);
   return (
     <div className="">
-      <Header />
+      <Highlight highlight={highlight} />
+      <Trending trending={trending} />
+      <Post post={post} />
+
+      {/* <Header />
       <div className="w-[2420px] h-[3005px] flex-col justify-start items-center gap-[100px] flex mx-auto p-[50px]">
         <div className="w-[2220px] h-[25px] justify-start items-center gap-10 flex pl-[20px]">
           <div className="text-orange-300 text-2xs font-bold font-['Work Sans'] leading-[25px]">
@@ -32,27 +36,33 @@ export default function Home(props) {
           </div>
         </div>
         <div className="w-[2216px] h-[1585px] flex-row justify-start items-center gap-8 flex flex-wrap">
-          {/* <img src={posts[3].cover_image}></img> */}
+          <img src={posts[3].cover_image}></img>
           {posts.map((el) => (
             <Card title={el.title} url={el.cover_image} />
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
 
-export const getStaticProps = async () => {
-  const res = await fetch("https://dev.to/api/articles?1");
-  const posts = await res.json();
+export const getStaticProps = async (context) => {
+  const highlights = await fetch(
+    "https://dev.to/api/articles?per_page=1&top=1"
+  );
+  const highlight = await highlights.json();
 
-  const res2 = await fetch("https://dev.to/api/articles?collection_id=99");
-  const posts2 = await res2.json();
+  const trendings = await fetch("https://dev.to/api/articles?per_page=5&top=1");
+  const trending = await trendings.json();
+
+  const posts = await fetch("https://dev.to/api/articles?per_page=15");
+  const post = await posts.json();
 
   return {
     props: {
-      posts,
-      posts2,
+      highlight,
+      post,
+      trending,
     },
   };
 };
